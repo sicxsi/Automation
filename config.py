780b4e602a8de19fd5427cd7f4874729f25df8d3
似曾相识 <48168591+sicxsi@users.noreply.github.com>
2024-12-12 17:04:04 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,4 @@
 # cookie 变量 文件
 #
 #
-sfsy ="https://mcs-mimp-web.sf-express.com/mcs-mimp/share/weChat/activityRedirect?source=CX&unionId=5aMqjxz1EbHeBw0rVIGv0BLeayhg93qFt2qe1pSQvbE%3D&openId=LnbnNqCi%2FHARLHObLd0waUWnssvqjizHV0VD4Q4d2bM%3D&memId=do1tv83A588mwe78guWDwVIxHib6pSfi4iNdb%2BkTpHIDdjSr%2F6X0WoiZtgGzs7sG&memNo=6tB15T6k0ZvyroDMcHJm6aCG0NccOj%2FM4XuBqG2SeBUDdjSr%2F6X0WoiZtgGzs7sG&mobile=7CwrmWH%2FeWz8TOqUq4sK8Q%3D%3D&mediaCode=wxapp&bizCode=%7B%22path%22%3A%22%2Fup-member%2FnewPoints%22%2C%22linkCode%22%3A%22SFAC20230803190840424%22%2C%22supportShare%22%3A%22YES%22%2C%22subCategoryCode%22%3A%221%22%2C%22from%22%3A%22mypoint%22%2C%22categoryCode%22%3A%221%22%7D"
-
-
-wx_hxek ="8a80818b936df41401937cb39b444f1e"
-
-app_5e ="17621912313#Zhang1022."
-linux ="2215981377@qq.com#Zhang1022."
-wx_hyhs ="122041#123456#123444"
+sfsy =""
