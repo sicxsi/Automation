eebf6ad1795b0a212086a9813f40f614f2bb887f
似曾相识 <48168591+sicxsi@users.noreply.github.com>
2024-12-22 01:00:57 +0800

    Add files via upload

--- Serv00.py
+++ Serv00.py
@@ -4,74 +4,79 @@
 # export Serv00=""
 # 换行分割 
 # 功能：登录保活
-# cron: 0 0 7 * *
+# cron: 0 0 0 */7 * 
 # new Env('Serv00 保活');
 
 import paramiko
-import time,re,os,sys
+import re
+import os
+import sys
 import config
 from notify import send
 
+def log_and_append(message):
+    msg.append(message)
+    print(message)
 
 msg = []
+
 def index(hostname, port, username, password):
     try:
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(hostname=hostname, port=port, username=username, password=password)
-        time.sleep(3)
-        msg.append("登录成功")
+        log_and_append("登录成功")
+        
         stdin, stdout, stderr = ssh.exec_command('ls')
-        msg.append("查看目录")
-        time.sleep(3)
-        print(stdout.read().decode())
-        time.sleep(3)
+        log_and_append("\n查看目录")
+        
+        output = stdout.read().decode()
+        log_and_append(output)
+        
         ssh.close()
         return True
     except paramiko.AuthenticationException:
-        msg.append("认证失败，用户名或密码错误")
+        log_and_append("认证失败，用户名或密码错误")
         return False
     except paramiko.SSHException as ssh_ex:
-        msg.append(f"SSH连接失败: {ssh_ex}")
+        log_and_append(f"SSH连接失败: {ssh_ex}")
         return False
     except Exception as ex:
-        msg.append(f"发生错误: {ex}")
+        log_and_append(f"发生错误: {ex}")
         return False
 
-
-def sicxs():
-    try:
-        env_cookie = os.environ.get("Serv00")
-        si_cookie = getattr(config, 'Serv00', '') 
-        if env_cookie and si_cookie:
-            cookies = env_cookie + "\n" + si_cookie
-        elif env_cookie:
-            cookies = env_cookie
-        elif si_cookie:
-            cookies = si_cookie
-        else:
-            msg.append("请设置变量 export Serv00='' 或在 config.py 中设置 Serv00 =")
-            sys.exit()
-    except Exception as e:
-        msg.append("请设置变量 export Serv00='' 或在 config.py 中设置 Serv00 =")
+def get_cookies():
+    env_cookie = os.environ.get("Serv00")
+    si_cookie = getattr(config, 'Serv00', '')
+    
+    if env_cookie and si_cookie:
+        return env_cookie + "\n" + si_cookie
+    elif env_cookie:
+        return env_cookie
+    elif si_cookie:
+        return si_cookie
+    else:
+        log_and_append("请设置变量 export Serv00='' 或在 config.py 中设置 Serv00 =")
         sys.exit()
 
-    list_cookie = re.split(r'\n|&', cookies)
-    total_cookies = len(list_cookie)
+def sicxs():
+    cookies = get_cookies()
+    list_cookies = re.split(r'\n|&', cookies)
+    total_cookies = len(list_cookies)
     
-    for i, list_cookie_i in enumerate(list_cookie):
+    for i, cookie in enumerate(list_cookies):
         try:
             print(f'\n----------- 账号【{i + 1}/{total_cookies}】执行 -----------')
-            list = list_cookie_i.split("#")
-            if index(list[0], list[1],list[2],list[3]):
-                msg.append(f"{list[2]} 执行保活任务成功")
+            host, port, user, pwd = cookie.split("#")
+            if index(host, int(port), user, pwd):
+                log_and_append(f"{user} 执行保活任务成功")
             else:
-                msg.append(f"{list[2]} 执行保活任务失败")    
+                log_and_append(f"{user} 执行保活任务失败")
         except Exception as e:
-            msg.append(f"账号【{i + 1}/{total_cookies}】执行出错")    
-        send("Serv00保号信息",msg)    
+            log_and_append(f"账号【{i + 1}/{total_cookies}】执行出错: {e}")
+    
     print(f'\n-----------  执 行  结 束 -----------')
 
-
 if __name__ == '__main__':
-   sicxs()
+    sicxs()
+    send("Serv00 保活", ''.join(msg))
